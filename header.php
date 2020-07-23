4c9435f9f07b4613732fbe09f14ae4c1ec6396f3
eddymens <kwasiamantin@gmail.com>
2020-07-24 01:59:52 +0200

    fix front page

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
             <!-- <a class="text-muted" href="#">Subscribe</a> -->
           </div>
           <div class="col-4 text-center">
-            <a class="blog-header-logo text-dark" href="#">Third National</a>
+            <a class="blog-header-logo text-dark" href="/">Third National</a>
           </div>
           <div class="col-4 d-flex justify-content-end align-items-center">
             <!-- <a class="text-muted" href="#">
@@ -43,4 +43,14 @@
             ); 
         ?>
       </div>
+<?php if(is_front_page()) { ?>
+  <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+        <div class="col-md-6 px-0">
+          <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
+          <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
+          <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+        </div>
+      </div>
+    </div>
+<?php } ?>
     </div>
