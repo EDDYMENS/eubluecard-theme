c763ba64e3988290ff6d766de19529662e931d97
eddymens <kwasiamantin@gmail.com>
2020-08-01 16:57:04 +0200

    add reword menu

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@
           the_post(); ?>
          <div class="blog-post">
             <h2 class="blog-post-title"><?php the_title(); ?></h2>
-            <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+            <p class="blog-post-meta"><?php the_date(); ?> by <?php the_author(); ?></p>
                 <?php the_content(); ?>
           </div><!-- /.blog-post -->
             <?php break;}
--- header.php
+++ header.php
@@ -26,7 +26,8 @@
             <!-- <a class="text-muted" href="#">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
             </a> -->
-            <a class="btn btn-sm btn-outline-secondary" href="/jobs/">Find a job</a>
+            <a class="btn btn-sm btn-outline-secondary" href="/jobs/">Find a job</a> &nbsp;
+            <a class="btn btn-sm btn-outline-secondary" href="/post-a-job/">Post a job</a>
           </div>
         </div>
       </header>
@@ -46,9 +47,9 @@
 <?php if(is_front_page()) { ?>
   <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic">Life in Europe</h1>
+          <h1 class="display-4 font-italic">Life with a Blue Card</h1>
           <p class="p-3"></p>
-          <p class="lead my-3">Learn about living and working in Europe as a third country national or foreigner</p>
+          <p class="lead my-3">Learn about living and working in Europe on a blue card</p>
         </div>
       </div>
     </div>
